e9f5d14c5eda14c6178d29c518c1fbddca9c4c76
Gabor Liptak <liptakgabor@freemail.hu>
2024-08-30 11:53:38 +0200

    Build environment is added

---
+++ config_lilen.bat
@@ -0,0 +1,11 @@
+::@set JAVA=C:\KBApps\openjdk1105
+::@set JAVA=C:\Program Files\Java\jdk1.8.0_152
+::@set JAVA=C:\KBApps\AdoptOpenJDK\
+::@set JAVA=C:\Program Files\Java\jre1.8.0_231
+::@set JAVA=C:\Program Files (x86)\Java\jdk1.7.0_11
+@set JAVA=C:\Programok\jdk-18.0.1.102-openj9
+
+::@set CYGWIN=C:\KBApps\DevEnv\Cygwin\V2_3_1
+@set CYGWIN=C:\Programok\cygwin64
+
+@set JAR_FILE=IotTerminal.jar
