d77ee5e03e5bc37f1e3076e7f84643ca1e7a3aff
Gabor Liptak <liptakgabor@freemail.hu>
2024-11-02 22:04:50 +0100

    Tester script: start option is configurable

--- _t_lilen.bat
+++ _t_lilen.bat
@@ -1,4 +1,6 @@
+@cls
+::@set START=start "%CD%"
 @PATH=%CD%\lib\RXTXcomm;%PATH%
 ::@set JAVA=C:\Program Files\Java\jre1.8.0_261
 @call config.bat
-"%JAVA%\bin\java" -jar bin\%JAR_FILE%
+%START% "%JAVA%\bin\java" -jar bin\%JAR_FILE%
