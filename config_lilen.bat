a7be4c8f77f3a4b796583914073d64559a585d72
Gabor Liptak <liptakgabor@freemail.hu>
2025-01-14 13:50:41 +0100

    New (x86 compatible) java versions are added for lilen

--- config_lilen.bat
+++ config_lilen.bat
@@ -1,9 +1,12 @@
 ::@set JAVA=C:\KBApps\openjdk1105
 ::@set JAVA=C:\Program Files\Java\jdk1.8.0_152
 ::@set JAVA=C:\KBApps\AdoptOpenJDK\
+::@set JAVA=C:\Programok\Java\x86\jdk-17.0.13+11
+@set JAVA=C:\Programok\Java\x86\jdk-19.0.2+7
 ::@set JAVA=C:\Program Files\Java\jre1.8.0_231
 ::@set JAVA=C:\Program Files (x86)\Java\jdk1.7.0_11
-@set JAVA=C:\Programok\jdk-18.0.1.102-openj9
+::@set JAVA=C:\Program Files (x86)\Java\jre1.8.0_161
+::@set JAVA=C:\Programok\jdk-18.0.1.102-openj9
 
 ::@set CYGWIN=C:\KBApps\DevEnv\Cygwin\V2_3_1
 @set CYGWIN=C:\Programok\cygwin64
