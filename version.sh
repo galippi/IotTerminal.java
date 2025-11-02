79ee759d2113c9acc8c92ad13ace4daa0fe57487
Gabor Liptak <liptakgabor@freemail.hu>
2025-11-02 10:42:20 +0100

    Version.sh improvement to get better version info in about dialog

--- version.sh
+++ version.sh
@@ -1,11 +1,31 @@
-# bash
-VERSION=`git rev-parse --short --verify HEAD 2>/dev/null`
-# VERSION=`git rev-parse --short --verify HEAD 2>/dev/null 1|tr -d '\n\r' `
-#VERSION=`git rev-parse --short --verify HEAD |tr -d '\n\r' `
-#echo VERSION=$VERSION mmm >_haha
-#echo $VERSION | tr -d '\n\r '>_haha2
-#VERSION=`cat _haha2`
-#echo VERSION=$VERSION mmm >_haha3
-rm -rf src/version/VersionInfo.java
-mkdir src/version
-sed s/git-revision/$VERSION/ <version.template > src/version/VersionInfo.java
+#!/bin/bash
+GIT_COMMIT_ID=`git rev-parse --short --verify HEAD 2>/dev/null`
+MODIFIED=`git status --short 2>/dev/null`
+LAST_TAG=`git describe --tags --exact-match 2>/dev/null`
+
+#echo "GIT_COMMIT_ID=$GIT_COMMIT_ID"
+#echo "MODIFIED=$MODIFIED"
+#echo "LAST_TAG=$LAST_TAG"
+
+if [ "$GIT_COMMIT_ID" == "" ]; then
+  GIT_COMMIT_ID='(git error)'
+else
+  if [ -z "$MODIFIED" ]; then
+    # not modified version
+    if [$LAST_TAG == '']; then
+      : # no tag - keep GIT_COMMIT_ID
+    else
+      GIT_COMMIT_ID=$LAST_TAG
+    fi
+  else
+    # modified content
+    GIT_COMMIT_ID="modified-$GIT_COMMIT_ID"
+  fi
+fi
+
+rm -rf src/version/VersionInfo.java 2>/dev/null
+mkdir src/version 2>/dev/null
+
+sed s/git-revision/$GIT_COMMIT_ID/ <version.template > src/version/VersionInfo.java
+
+echo "$0: Resulted GIT_COMMIT_ID=$GIT_COMMIT_ID"
