08b1e1cb1ee53321ef86af39dae842e82b378db1
Gabor Liptak <liptakgabor@freemail.hu>
2025-11-05 20:40:03 +0100

    Device setup table is prepared - step 1

--- src/IotTerminal/IotDeviceSetupDlg.java
+++ src/IotTerminal/IotDeviceSetupDlg.java
@@ -1,5 +1,6 @@
 package IotTerminal;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Point;
@@ -12,11 +13,24 @@ import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.KeyStroke;
 import javax.swing.SpringLayout;
 import javax.swing.SwingConstants;
 
 import lippiWare.utils.dbg;
+
+class IotDeviceTable extends JPanel {
+    IotDeviceTable() {
+        super(new BorderLayout());
+        JPanel myTable = new JPanel();
+        JScrollPane scrollableTable = new JScrollPane(myTable);
+        scrollableTable.setMinimumSize(new Dimension(350, 200));
+        add(scrollableTable);
+    }
+
+    private static final long serialVersionUID = 7799538399508125604L;
+}
 
 public class IotDeviceSetupDlg extends JDialog {
 
@@ -24,8 +38,18 @@ public class IotDeviceSetupDlg extends JDialog {
         parent = _parent;
         this.setTitle("Device setup");
 
-        JPanel myTable = new JPanel();
-        JScrollPane scrollableTable = new JScrollPane(myTable);
+        IotDeviceTable scrollableTable = new IotDeviceTable();
+
+        JPanel channelsPanel = new JPanel();
+        //channelsPanel.setMinimumSize(new Dimension(350, 200));
+        JScrollPane channelsPane = new JScrollPane(channelsPanel);
+        channelsPane.setMinimumSize(new Dimension(350, 200));
+
+        jsp = new JSplitPane(JSplitPane.VERTICAL_SPLIT, scrollableTable , channelsPane);
+        jsp.setDividerLocation(IotTerminalPrefs.get("DeviceSetupVerticalSplit", 200));
+        JPanel horizontalSplit = new JPanel(new BorderLayout());
+        horizontalSplit.add(jsp);
+        horizontalSplit.setMinimumSize(new Dimension(350, 400));
 
         JButton bOk = new JButton("OK");
         bOk.setHorizontalAlignment(SwingConstants.LEFT);
@@ -52,16 +76,17 @@ public class IotDeviceSetupDlg extends JDialog {
         SpringLayout layout = new SpringLayout();
         cp.setLayout(layout);
 
-        cp.add(scrollableTable);
+        cp.add(horizontalSplit);
         cp.add(bOkCancel);
 
-        layout.putConstraint(SpringLayout.WEST,  scrollableTable,  5, SpringLayout.WEST,  cp);
-        layout.putConstraint(SpringLayout.EAST,  scrollableTable, -5, SpringLayout.EAST,  cp);
-        layout.putConstraint(SpringLayout.NORTH, scrollableTable,  5, SpringLayout.SOUTH, cp);
-        layout.putConstraint(SpringLayout.SOUTH, scrollableTable, -5, SpringLayout.NORTH, bOkCancel);
+        layout.putConstraint(SpringLayout.WEST,  horizontalSplit,  5, SpringLayout.WEST,  cp);
+        layout.putConstraint(SpringLayout.EAST,  horizontalSplit, -5, SpringLayout.EAST,  cp);
+        layout.putConstraint(SpringLayout.NORTH, horizontalSplit,  5, SpringLayout.NORTH, cp);
+        layout.putConstraint(SpringLayout.SOUTH, horizontalSplit, -5, SpringLayout.NORTH, bOkCancel);
 
-        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER,  bOkCancel,  0, SpringLayout.HORIZONTAL_CENTER,  cp);
-        layout.putConstraint(SpringLayout.SOUTH, bOkCancel, -5, SpringLayout.SOUTH, cp);
+        layout.putConstraint(SpringLayout.NORTH,             bOkCancel, -40, SpringLayout.SOUTH,             cp);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, bOkCancel,   0, SpringLayout.HORIZONTAL_CENTER, cp);
+        layout.putConstraint(SpringLayout.SOUTH,             bOkCancel,  -5, SpringLayout.SOUTH,             cp);
 
         pack();
 
@@ -108,8 +133,10 @@ public class IotDeviceSetupDlg extends JDialog {
         IotTerminalPrefs.put("DeviceSetupDialogY", getY());
         IotTerminalPrefs.put("DeviceSetupDialogH", getHeight());
         IotTerminalPrefs.put("DeviceSetupDialogW", getWidth());
+        IotTerminalPrefs.put("DeviceSetupVerticalSplit", jsp.getDividerLocation());
     }
 
     IotTerminalMain parent;
+    JSplitPane jsp;
     private static final long serialVersionUID = 8080722709719513891L;
 }
